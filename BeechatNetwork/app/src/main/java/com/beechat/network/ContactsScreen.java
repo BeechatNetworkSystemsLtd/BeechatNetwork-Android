6aa6bd79fdce5f24535f4b79d2a40f3bf1b5af32
uladzislaudubouski <udubouski@yandex.ru>
2021-11-12 00:03:02 +0300

    conversations-window-changes - Updated UI for the conversations-window-changes

--- BeechatNetwork/app/src/main/java/com/beechat/network/ContactsScreen.java
+++ BeechatNetwork/app/src/main/java/com/beechat/network/ContactsScreen.java
@@ -1,20 +1,25 @@
 package com.beechat.network;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.digi.xbee.api.android.DigiMeshDevice;
+import com.digi.xbee.api.exceptions.XBeeException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +35,12 @@ public class ContactsScreen extends Fragment {
     public static List<String> contacts = new ArrayList<>();
     View view;
     ListView contactsListView;
+    public static String senderId = null;
+    public static String receiverId = null;
 
+    public static List<String> xbee_names = new ArrayList<>();
+    public static List<String> names = new ArrayList<>();
+    private static String selectedDevice = null;
 
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -49,6 +59,43 @@ public class ContactsScreen extends Fragment {
         contactsListView = (ListView) view.findViewById(R.id.contactsListView);
         remoteXBeeDeviceAdapterName = new CustomContactAdapter(getActivity(), contacts);
         contactsListView.setAdapter(remoteXBeeDeviceAdapterName);
+
+        // Handling an event on clicking an item from the list of available devices.
+        contactsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Reading all users
+                System.out.println("Reading: " + "Reading all users..");
+                List<User> users = SplashScreen.db.getAllUsers();
+
+
+                for (User cn : users) {
+                    xbee_names.add(cn.getXbeeDeviceNumber());
+                    names.add(cn.getName());
+                }
+
+                senderId = NearbyDevicesScreen.getDMDevice().toString();
+                selectedDevice = senderId;//remoteXBeeDeviceAdapter.getItem(i);
+                receiverId = selectedDevice;
+
+                if (xbee_names.isEmpty()) {
+                    System.out.println("Account receiverId " + receiverId + " not exist!");
+                    Intent intent = new Intent(getActivity(), AddContactScreen.class);
+                    intent.putExtra("key", receiverId);
+                    startActivity(intent);
+                } else {
+                    if (xbee_names.contains(receiverId)) {
+                        System.out.println("Account receiverId " + receiverId + " exist!");
+                        connectToDevice(selectedDevice);
+                    } else {
+                        System.out.println("Account receiverId " + receiverId + " not exist!");
+                        Intent intent = new Intent(getActivity(), AddContactScreen.class);
+                        intent.putExtra("key", receiverId);
+                        startActivity(intent);
+                    }
+                }
+            }
+        });
         return view;
     }
 
@@ -88,5 +135,44 @@ public class ContactsScreen extends Fragment {
         super.onResume();
         remoteXBeeDeviceAdapterName.notifyDataSetChanged();
     }
+    /***
+     *  --- connectToDevice(String) ----
+     *  The function of establishing a connection with the selected device.
+     *
+     *  @param device Selected device number.
+     ***/
+    private void connectToDevice(final String device) {
+        final ProgressDialog dialog = ProgressDialog.show(getActivity(), getResources().getString(R.string.connecting_device_title),
+                getResources().getString(R.string.connecting_device_description), true);
 
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    NearbyDevicesScreen.myDevice.open();
+
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.dismiss();
+                            Intent intent = new Intent(getActivity(), ChatScreen.class);
+                            startActivity(intent);
+                        }
+                    });
+                } catch (final XBeeException e) {
+                    e.printStackTrace();
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            dialog.dismiss();
+                            new AlertDialog.Builder(getActivity()).setTitle(getResources().getString(R.string.error_connecting_title))
+                                    .setMessage(getResources().getString(R.string.error_connecting_description, e.getMessage()))
+                                    .setPositiveButton(android.R.string.ok, null).show();
+                        }
+                    });
+                    NearbyDevicesScreen.myDevice.close();
+                }
+            }
+        }).start();
+    }
 }
